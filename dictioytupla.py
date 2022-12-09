ab5943eaf287328f191b3fdd1fd2541438ef1887
lauralardies <91721558+lauralardies@users.noreply.github.com>
2022-12-09 16:09:54 +0000

    pt 1

---
+++ dictioytupla.py
@@ -0,0 +1,14 @@
+from collections import defaultdict, OrderedDict
+
+d = defaultdict(float)
+print(d['algo'])
+print(d)
+
+d = defaultdict(str)
+print(d['algo'])
+print(d)
+
+d = defaultdict(object)
+print(d['algo'])
+print(d)
+
