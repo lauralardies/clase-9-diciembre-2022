a1d91e82295f7e31936581924e94fd82f159f09e
lauralardies <lrodrlar@myuax.com>
2022-12-09 16:38:04 +0100

    cambios

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="paquete",
+    version="0.1",
+    description="Este es un paquete de jemplo",
+    author="Hector Costa",
+    author_email="hola@hektorprofe.com",
+    url="http://www.hektorprofe.net",
+    packages=['paquete','paquete.hola','paquete.adios']
+    scripts=[]
+)
+
